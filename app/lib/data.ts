8e601cccd28c50e524e15215ed747d00395385f8
Bi <116228374+bianny1107@users.noreply.github.com>
2023-11-26 22:31:07 -0400

    feat: Fetch the last 5 invoices
    Part of chapter 7.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -33,6 +33,12 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+const data = await sql<LatestInvoiceRaw>`
+  SELECT invoices.amount, customers.name, customers.image_url, customers.email
+  FROM invoices
+  JOIN customers ON invoices.customer_id = customers.id
+  ORDER BY invoices.date DESC
+  LIMIT 5`;
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
